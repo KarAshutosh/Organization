cbc74b7c88eadf8bda3e6d4898db02ae2f686da0
Ashutosh K <81259418+KarAshutosh@users.noreply.github.com>
2022-06-14 11:53:44 +0530

    Update managers.sol

--- managers.sol
+++ managers.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Managers is Ownable
 {
-    address[20] public managers;
+    address[] public managers;
     uint8 public managerCount = 0; 
 
     function addManager(address _newManager)  public onlyOwner
@@ -15,7 +15,8 @@ contract Managers is Ownable
         managerCount = managerCount + 1;
     }        
 
-    function removeManager(address _oldManager)  public onlyOwner
+    /*
+    function removeManagerByAddress(address _oldManager)  public onlyOwner
     {
         for(uint8 i = 0; i < managerCount; i++)
         {
@@ -29,12 +30,23 @@ contract Managers is Ownable
             }
         }
     }   
+    */
+
+    function removeManagerByID(uint8 _ManagerID) public onlyOwner
+    {
+        uint8 lastManager = managerCount - 1;
+        //oldManagerID = _ManagerID
+        managers[_ManagerID] = managers[lastManager];
+        managers.pop();
+        managerCount = managerCount - 1;
+    }
+
 
 
     modifier onlyManagers
     {
         bool isManager = false;
-        
+
         for(uint8 i = 0; i < managerCount; i++)
         {
             if(managers[i] == _msgSender())
@@ -46,5 +58,29 @@ contract Managers is Ownable
 
         require(isManager = true, "Wallet not authorised");
         _;
+    }
+
+    modifier onlyOwnerAndManager
+    {
+        bool isManager = false;
+
+        for(uint8 i = 0; i < managerCount; i++)
+        {
+            if(owner() == _msgSender())
+            {
+                isManager = true;
+                break;
+            }
+
+            if(managers[i] == _msgSender())
+            {
+                isManager = true;
+                break;
+            }
+        }
+
+        require(isManager = true, "Wallet not authorised");
+        _;
+
     }
 }
